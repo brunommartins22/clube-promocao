7d45833b479406465b68487a7dd3c4c7ca84d2af
josev.junior <josev.junior@outlook.com>
2019-11-19 22:54:18 -0300

    Alteraçoes da semana passada

--- src/app/pages/erros/erro-404.ts
+++ src/app/pages/erros/erro-404.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-erro-404',
-  templateUrl: './page-erro-404.html'
+  templateUrl: './erro-404.html'
 })
 export class Erro404Component implements OnInit {
 
